041507bbd32ae63ccf694c2ea9437fe9e87793ea
Nuno Caneco <nuno.caneco@gmail.com>
2019-10-17 00:06:43 +0100

    feat: Add wordconnect/find endpoint
    Issue #2

--- webapi/src/main/kotlin/com/nmcc/wordconnect/webapi/WebapiApplication.kt
+++ webapi/src/main/kotlin/com/nmcc/wordconnect/webapi/WebapiApplication.kt
@@ -1,10 +1,23 @@
 package com.nmcc.wordconnect.webapi
 
+import com.nmcc.wordconnect.webapi.engine.reader.FlatFileWordReader
+import com.nmcc.wordconnect.webapi.engine.reader.IWordReader
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.io.FileNotFoundException
 
 @SpringBootApplication
-class WebapiApplication
+class WebapiApplication {
+
+	@Bean
+	fun wordReader(): IWordReader {
+		val englishDictionary = this.javaClass.classLoader.getResourceAsStream("words-en.txt")
+				?: throw FileNotFoundException("Unable to find dictionary")
+
+		return FlatFileWordReader(englishDictionary)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<WebapiApplication>(*args)
